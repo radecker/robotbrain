4f0a65c55da381767a6b65df381ed7fb1be5f8e2
Ryan <Ryan>
2017-08-23 21:32:06 -0500

    Initial commit with all previous progress

---
+++ messages.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""
+Summary:    This file is meant to establish the message header format and can be called to create a message header
+
+Created:    July 27th, 2017
+Author:     Ryan Decker
+
+Change Log:
+
+"""
+
+
+import time
+
+def addHeader(message_type):
+    header = []
+    syncMsg = ["DE", "AD", "BE", "EF"]
+
+    message_type = hex(int(message_type))[2:4]
+    if len(message_type) < 2:
+        message_type = "0" + message_type
+    for word in syncMsg:
+        header.append(word)
+    header.append(message_type)
+    for time_word in time.gmtime():
+        time_word = hex(time_word)[2:4]
+        header.append(time_word)
+    while len(header) < 16:
+        header.append("00")
+    for index in range(0, len(header)):
+        if len(header[index]) < 2:
+            header[index] = "0" + header[index]
+    header_string = ' '.join(header)
+    header_string += " "
+    return header_string
+
+def addBuffer(message):
+    message_array = message.split(' ')
+    while len(message_array) < 60:
+        message_array.append("00")
+    message = " ".join(message_array)
+    return message
---
+++ serialComm.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+Summary:    This file will initiate, transmit, and receive serial communications between the raspberry pi and the
+            arduino.
+
+Created:    July 27th, 2017
+Author:     Ryan Decker
+
+Change Log:
+
+"""
+
+import messages
+import time
+import serial
+
+def serialInit(port, baud_rate):
+    ser = serial.Serial(port, baud_rate)
+    return ser
+
+def sendMsg(message, message_type):
+    header = messages.addHeader(message_type)
+    message = header + message + messages.addBuffer(message)
+    message = message.split(' ')
+    print(message)
+    #bytes_written = ser.write(message)
+    #return bytes_written
+
+def readMsg(num_messages):
+    message = []
+    while len(message) < num_messages:
+        num_bytes = 0
+        while not num_bytes:
+            num_bytes = ser.inWaiting()
+            if num_bytes:
+                time.sleep(.1)
+                num_bytes = ser.inWaiting()
+        message.append(ser.read(num_bytes))
+    return message
+
+def serialClose():
+    ser.close()
+
+
+ser = serialInit("COM8", 9600)
+sendMsg("FF FF FF", 3)
+print(readMsg(4))
+
+serialClose()
